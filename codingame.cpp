3a89859875daeec8420177bc551fbbf1a01b3253
Chipppppppppp <78024852+Chipppppppppp@users.noreply.github.com>
2022-11-29 00:08:49 +0900

    First commit

---
+++ codingame.cpp
@@ -0,0 +1,170 @@
+#pragma GCC target("avx512f")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include <vector>
+#include "state.hpp"
+
+State state;
+
+std::unordered_map<State, State::Position> book;
+
+struct BookInit {
+    static constexpr const char* book_str = "f5\nf5d6\nf5d6c3g5\nf5d6c3g5c6c5\nf5d6c3g5c6c5c4b6\nf5d6c3g5c6c5c4b6f6f4\nf5d6c3g5c6c5c4b6f6f4e6d7\nf5d6c3g5c6c5c4b6f6f4e6d7c7g6\nf5d6c3g5c6c5c4b6f6f4e6d7c7g6d8b5\nf5d6c3g5c6c5c4b6f6f4e6d7c7g6d8b5e7b3\nf5d6c3g5c6c5c4b6f6f4e6d7c7g6d8b5e7b3a6e3\nf5d6c3g5c6c5c4b6f6f4e6d7c7g6d8b5e7b3a6e3a5d3\nf5d6c3g5f6d3\nf5d6c3g5f6d3e3c2\nf5d6c3g5f6d3e3c2c1e6\nf5d6c3g5f6d3e3c2c1e6f4f3\nf5d6c3g5f6d3e3c2c1e6f4f3f2g4\nf5d6c3g5f6d3e3c2c1e6f4f3f2g4g6d2\nf5d6c3g5f6d3e3c2c1e6f4f3f2g4g6d2h3h4\nf5d6c3g5f6d3e3c2c1e6f4f3f2g4g6d2h3h4h5f7\nf5d6c3g5f6d3e3c2c1e6f4f3f2g4g6d2h3h4h5f7e7g3\nf5d6c3g5g6d3\nf5d6c3g5g6d3c4e3\nf5d6c3g5g6d3c4e3f3b4\nf5d6c3g5g6d3c4e3f3b4f6e6\nf5d6c3g5g6d3c4e3f3b4f6e6f4g4\nf5d6c3g5g6d3c4e3f3b4f6e6f4g4h4h5\nf5d6c3g5g6d3c4e3f3b4f6e6f4g4h4h5h6g3\nf5d6c3g5g6d3c4e3f3b4f6e6f4g4h4h5h6g3h3f7\nf5d6c3g5g6d3c4e3f3b4f6e6f4g4h4h5h6g3h3f7f8c2\nf5d6c4b3\nf5d6c4b3b4f4\nf5d6c4b3b4f4f6g5\nf5d6c4b3b4f4f6g5f3e7\nf5d6c4b3b4f4f6g5f3e7c5e6\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3h3e3\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3h3e3f2b6\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3h3e3f2b6h4d3\nf5d6c5b4\nf5d6c5b4d7e7\nf5d6c5b4d7e7c7d8\nf5d6c5b4d7e7c7d8c3d3\nf5d6c5b4d7e7c7d8c3d3c4b3\nf5d6c5b4d7e7c7d8c3d3c4b3d2e2\nf5d6c5b4d7e7c7d8c3d3c4b3d2e2c2e3\nf5d6c5b4d7e7c7d8c3d3c4b3d2e2c2e3f4f2\nf5d6c5b4d7e7c7d8c3d3c4b3d2e2c2e3f4f2c6b5\nf5d6c5b4d7e7c7d8c3d3c4b3d2e2c2e3f4f2c6b5f3c8\nf5d6c4\nf5d6c4b3b4\nf5d6c4b3b4f4f6\nf5d6c4b3b4f4f6g5f3\nf5d6c4b3b4f4f6g5f3e7c5\nf5d6c4b3b4f4f6g5f3e7c5e6c3\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3h3\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3h3e3f2\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3h3e3f2b6h4\nf5d6c4b3b4f4f6g5f3e7c5e6c3g4c6g3h3e3f2b6h4d3e2\nf5d6c4d3c3\nf5d6c4d3c3b3d2\nf5d6c4d3c3b3d2e1b5\nf5d6c4d3c3b3d2e1b5c5b4\nf5d6c4d3c3b3d2e1b5c5b4e3c2\nf5d6c4d3c3b3d2e1b5c5b4e3c2a4c6\nf5d6c4d3c3b3d2e1b5c5b4e3c2a4c6d1e2\nf5d6c4d3c3b3d2e1b5c5b4e3c2a4c6d1e2c7b6\nf5d6c4d3c3b3d2e1b5c5b4e3c2a4c6d1e2c7b6f1e6\nf5d6c4d3c3b3d2e1b5c5b4e3c2a4c6d1e2c7b6f1e6f3f2\nf5d6c4d3c3f4f6\nf5d6c4d3c3f4f6f3e6\nf5d6c4d3c3f4f6f3e6e7f7\nf5d6c4d3c3f4f6f3e6e7f7c5b6\nf5d6c4d3c3f4f6f3e6e7f7c5b6g5e3\nf5d6c4d3c3f4f6f3e6e7f7c5b6g5e3d7c6\nf5d6c4d3c3f4f6f3e6e7f7c5b6g5e3d7c6e2g4\nf5d6c4d3c3f4f6f3e6e7f7c5b6g5e3d7c6e2g4h3d2\nf5d6c4d3c3f4f6f3e6e7f7c5b6g5e3d7c6e2g4h3d2g3f1\nf5d6c4d3c3f4f6f3e6e7f7c5b6g6e3\nf5d6c4d3c3f4f6f3e6e7f7c5b6g6e3e2f1\nf5d6c4d3c3f4f6f3e6e7f7c5b6g6e3e2f1d1g5\nf5d6c4d3c3f4f6f3e6e7f7c5b6g6e3e2f1d1g5c6d8\nf5d6c4d3c3f4f6f3e6e7f7c5b6g6e3e2f1d1g5c6d8g4h6\nf5d6c4d3c3f4f6b4c2\nf5d6c4d3c3f4f6b4c2f3e3\nf5d6c4d3c3f4f6b4c2f3e3e2c6\nf5d6c4d3c3f4f6b4c2f3e3e2c6f2c5\nf5d6c4d3c3f4f6b4c2f3e3e2c6f2c5e6d2\nf5d6c4d3c3f4f6b4c2f3e3e2c6f2c5e6d2g4d7\nf5d6c4d3c3f4f6b4c2f3e3e2c6f2c5e6d2g4d7b3g5\nf5d6c4d3c3f4f6b4c2f3e3e2c6f2c5e6d2g4d7b3g5c8h4\nf5d6c4d3c3f4f6g5e3\nf5d6c4d3c3f4f6g5e3f3g6\nf5d6c4d3c3f4f6g5e3f3g6e2h5\nf5d6c4d3c3f4f6g5e3f3g6e2h5c5g4\nf5d6c4d3c3f4f6g5e3f3g6e2h5c5g4g3f2\nf5d6c4d3c3b5b4\nf5d6c4d3c3b5b4f4c5\nf5d6c4d3c3b5b4f4c5a4b3\nf5d6c4d3c3b5b4f4c5a4b3d2a6\nf5d6c4d3c3b5b4f4c5a4b3d2a6a3e3\nf5d6c4d3c3b5b4f4c5a4b3d2a6a3e3f3g4\nf5d6c4d3c3b5b4f4c5a4b3d2a6a3e3f3g4e6f6\nf5d6c4d3c3b5b4f4c5a4b3d2a6a3e3f3g4e6f6g3e2\nf5d6c4d3c3b5b4f4c5a4b3d2a6a3e3f3g4e6f6g3e2c2f2\nf5d6c4g5f6\nf5d6c4g5f6f4f3\nf5d6c4g5f6f4f3d3c3\nf5d6c4g5f6f4f3d3c3g6e3\nf5d6c4g5f6f4f3d3c3g6e3e6h5\nf5d6c4g5f6f4f3d3c3g6e3e6h5d2e2\nf5d6c4g5f6f4f3d3c3g6e3e6h5d2e2c2c6\nf5d6c4g5f6f4f3d3c3g6e3e6h5d2e2c2c6c5b6\nf5d6c4g5f6f4f3d3c3g6e3e6h5d2e2c2c6c5b6b4b3\nf5d6c4g5f6f4f3d3c3g6e3e6h5d2e2c2c6c5b6b4b3c7a4\nf5f6e6\nf5f6e6f4g6\nf5f6e6f4g6c5f3\nf5f6e6f4g6c5f3g4e3\nf5f6e6f4g6c5f3g4e3d6g5\nf5f6e6f4g6c5f3g4e3d6g5g3c3\nf5f6e6f4g6c5f3g4e3d6g5g3c3h5c4\nf5f6e6f4g6c5f3g4e3d6g5g3c3h5c4d7h6\nf5f6e6f4g6c5f3g4e3d6g5g3c3h5c4d7h6h7h3\nf5f6e6f4g6c5f3g4e3d6g5g3c3h5c4d7h6h7h3f7e7\nf5f6e6f4g6c5f3g4e3d6g5g3c3h5c4d7h6h7h3f7e7f8h4\nf5f6e6f4g6c5f3g5d6\nf5f6e6f4g6c5f3g5d6e3h4\nf5f6e6f4g6c5f3g5d6e3h4g3g4\nf5f6e6f4g6c5f3g5d6e3h4g3g4h6e2\nf5f6e6f4g6c5f3g5d6e3h4g3g4h6e2d3h5\nf5f6e6f4g6c5f3g5d6e3h4g3g4h6e2d3h5h3c6\nf5f6e6f4g6c5f3g5d6e3h4g3g4h6e2d3h5h3c6e7f2\nf5f6e6f4g6c5f3g5d6e3h4g3g4h6e2d3h5h3c6e7f2c4d2\nf5f6e6f4g6d6g4\nf5f6e6f4g6d6g4g5h4\nf5f6e6f4g6d6g4g5h4e7f3\nf5f6e6f4g6d6g4g5h4e7f3h6f7\nf5f6e6f4g6d6g4g5h4e7f3h6f7e8f8\nf5f6e6f4g6d6g4g5h4e7f3h6f7e8f8g8d3\nf5f6e6f4g6d6g4g5h4e7f3h6f7e8f8g8d3h5h7\nf5f6e6f4g6d6g4g5h4e7f3h6f7e8f8g8d3h5h7e3c5\nf5f6e6f4g6d6g4g5h4e7f3h6f7e8f8g8d3h5h7e3c5c4g3\nf5f6e6d6f7\nf5f6e6d6f7e3c6\nf5f6e6d6f7e3c6e7f4\nf5f6e6d6f7e3c6e7f4c5d8\nf5f6e6d6f7e3c6e7f4c5d8c7d7\nf5f6e6d6f7e3c6e7f4c5d8c7d7f8b5\nf5f6e6d6f7e3c6e7f4c5d8c7d7f8b5c4e8\nf5f6e6d6f7e3c6e7f4c5d8c7d7f8b5c4e8c8f3\nf5f6e6d6f7e3c6e7f4c5d8c7d7f8b5c4e8c8f3g5b6\nf5f6e6d6f7e3c6e7f4c5d8c7d7f8b5c4e8c8f3g5b6d3b4\nf5f6e6d6f7f4d7\nf5f6e6d6f7f4d7e7d8\nf5f6e6d6f7f4d7e7d8g5c6\nf5f6e6d6f7f4d7e7d8g5c6f8g6\nf5f6e6d6f7f4d7e7d8g5c6f8g6h5h6\nf5f6e6d6f7f4d7e7d8g5c6f8g6h5h6h7c4\nf5f6e6d6f7f4d7e7d8g5c6f8g6h5h6h7c4e8g8\nf5f6e6d6f7f4d7e7d8g5c6f8g6h5h6h7c4e8g8c5e3\nf5f6e6d6f7f4d7e7d8g5c6f8g6h5h6h7c4e8g8c5e3d3c7\n";
+    BookInit() {
+        std::stringstream st(book_str);
+        std::string line;
+        State state;
+        int n = 0;
+        int x, y;
+        for (; ; ) {
+            std::getline(st, line);
+            if (st.eof()) break;
+            ++n;
+            state = State{};
+            for (int i = 0; i < (int)line.size() - 2; i += 2) {
+                x = (int)(line[i]) - (int)'a';
+                y = (int)(line[i + 1]) - (int)'1';
+                state.move({x, y});
+            }
+            x = (int)(line[line.size() - 2]) - (int)'a';
+            y = (int)(line[line.size() - 1]) - (int)'1';
+            book[state] = {x, y};
+
+            state = State{};
+            for (int i = 0; i < (int)line.size() - 2; i += 2) {
+                x = (int)(line[i]) - (int)'a';
+                y = (int)(line[i + 1]) - (int)'1';
+                state.move({y, x});
+            }
+            x = (int)(line[line.size() - 2]) - (int)'a';
+            y = (int)(line[line.size() - 1]) - (int)'1';
+            book[state] = {y, x};
+
+            state = State{};
+            for (int i = 0; i < (int)line.size() - 2; i += 2) {
+                x = (int)(line[i]) - (int)'a';
+                y = (int)(line[i + 1]) - (int)'1';
+                state.move({7 - x, 7 - y});
+            }
+            x = (int)(line[line.size() - 2]) - (int)'a';
+            y = (int)(line[line.size() - 1]) - (int)'1';
+            book[state] = {7 - x, 7 - y};
+
+            state = State{};
+            for (int i = 0; i < (int)line.size() - 2; i += 2) {
+                x = (int)(line[i]) - (int)'a';
+                y = (int)(line[i + 1]) - (int)'1';
+                state.move({7 - y, 7 - x});
+            }
+            x = (int)(line[line.size() - 2]) - (int)'a';
+            y = (int)(line[line.size() - 1]) - (int)'1';
+            book[state] = {7 - y, 7 - x};
+        }
+    }
+} book_init;
+
+clock_t start_time;
+int result_a, result_b;
+template<int time_limit, bool first = true>
+int alpha_beta(const State& state, int depth, int a, int b, State::Position& pos) {
+    if (clock() - start_time >= time_limit) return 0;
+    if (depth == 0) return state.eval();
+    bool flag = true;
+    for (int i = 0; i < 64; ++i) if (State::Position p{i}; state.is_putable(state.turn, p)) {
+        flag = false;
+        int res = -alpha_beta<time_limit, false>(state.moved(p), depth - 1, -b, -a, pos);
+        if (clock() - start_time >= time_limit) return 0;
+        if (a < res) {
+            a = res;
+            if constexpr (first) pos = p;
+        }
+        if (a >= b) return a;
+    }
+    if (flag) {
+        if (state.any_putable(state.turn.next())) return -alpha_beta<time_limit, false>(state.passed(), depth - 1, -b, -a, pos);
+        else return state.eval();
+    }
+    return a;
+}
+
+constexpr int time_limit = CLOCKS_PER_SEC / 8;
+constexpr int last_stage_time_limit = time_limit / 2;
+std::pair<State::Position, int> alpha_beta_move() noexcept {
+    start_time = clock();
+    State::Position pos;
+    int res;
+    if (state.count_all_stone() >= 54) {
+        res = alpha_beta<last_stage_time_limit>(state, inf, -inf, inf, pos);
+        if (clock() - start_time < last_stage_time_limit) return {pos, res};
+    }
+    State::Position pos_before;
+    int res_before;
+    for (int depth = 3; ; ++depth) {
+        res = alpha_beta<time_limit>(state, depth, -inf, inf, pos);
+        if (clock() - start_time >= time_limit) return {pos_before, res_before};
+        pos_before = pos;
+        res_before = res;
+    }
+}
+
+void cpu_move() {
+    State::Position pos;
+    auto itr = book.find(state);
+    if (itr != book.end()) {
+        pos = itr->second;
+        std::cout << (char)('a' + pos.y) << pos.x + 1 << " MSG " << "formula" << '\n';
+    } else {
+        auto [pos, res] = alpha_beta_move();
+        std::cout << (char)('a' + pos.y) << pos.x + 1 << " MSG " << res << '\n';
+    }
+    fflush(stdout);
+    state.move(pos);
+}
+
+void input_move() {
+    std::string s;
+    bool flag = false;
+    for (int i = 0; i < 8; ++i) {
+        std::cin >> s;
+        for (int j = 0; j < 8; ++j) if (s[j] != '.' && !state.exists(State::black, {i, j}) && !state.exists(State::white, {i, j})) {
+            state.move({i, j});
+            state.pass();
+            flag = true;
+        }
+    }
+    if (flag) state.pass();
+    int action_count;
+    std::cin >> action_count;
+    for (int i = 0; i < action_count; ++i) std::cin >> s;
+}
+
+int main() {
+    bool my_turn;
+    std::cin >> my_turn;
+    std::string s;
+    std::cin >> s;
+    if (!my_turn) {
+        for (int i = 0; i < 8; ++i) std::cin >> s;
+        int action_count;
+        std::cin >> action_count;
+        for (int i = 0; i < action_count; ++i) std::cin >> s;
+    }
+    for (; ; ) {
+        if (!state.any_putable(state.turn)) {
+            if (!state.any_putable(state.turn.next())) break;
+            state.pass();
+            continue;
+        }
+        if ((bool)state.turn == my_turn) cpu_move();
+        else input_move();
+    }
+}
---
+++ state.hpp
@@ -0,0 +1,349 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <utility>
+#include <vector>
+
+// 無限大
+inline constexpr int inf = std::numeric_limits<int>::max() / 3;
+
+struct State {
+    // ターンのクラス
+    struct Turn {
+        // falseの時黒
+        bool color;
+
+        Turn() noexcept = default;
+        Turn(bool color) noexcept: color(color) {}
+
+        // boolに変換
+        explicit operator bool() const noexcept {
+            return color;
+        }
+
+        // デバッグ用
+        std::string str() const {
+            return color ? "white" : "black";
+        }
+
+        // 黒か
+        bool is_black() const noexcept {
+            return !color;
+        }
+
+        // 白か
+        bool is_white() const noexcept {
+            return color;
+        }
+
+        // 比較
+        bool operator ==(const Turn& stone) const noexcept {
+            return color == stone.color;
+        }
+        bool operator !=(const Turn& stone) const noexcept {
+            return color != stone.color;
+        }
+
+        // 次のターン
+        void toggle() noexcept {
+            color = !color;
+        }
+
+        // 次のターンを返す
+        Turn next() const noexcept {
+            return Turn{!color};
+        }
+    };
+
+    inline static const Turn black{false}, white{true};
+
+    // 位置を表すクラス
+    struct Position {
+        int x, y;
+
+        Position() noexcept = default;
+        Position(int x, int y) noexcept: x(x), y(y) {}
+        explicit Position(int t) noexcept: x(t >> 3), y(t & 7) {}
+
+        // 比較
+        bool operator ==(const Position& pos) const noexcept {
+            return x == pos.x && y == pos.y;
+        }
+        bool operator !=(const Position& pos) const noexcept {
+            return !(*this == pos);
+        }
+
+        // 範囲内か調べる
+        bool is_in_range() const noexcept {
+            return 0 <= x && x < 8 && 0 <= y && y < 8;
+        }
+
+        // bit boardアクセス用
+        int flatten() const noexcept {
+            return (x << 3) + y;
+        }
+    };
+
+    // 角
+    inline static const std::array<Position, 4> corners{{{0, 0}, {0, 7}, {7, 0}, {7, 7}}};
+
+    // 方向を表すクラス
+    struct Direction {
+        int dx, dy;
+
+        Direction() noexcept = default;
+        Direction(int dx, int dy) noexcept: dx(dx), dy(dy) {}
+
+        // 比較
+        bool operator ==(const Direction& d) const noexcept {
+            return dx == d.dx && dy == d.dy;
+        }
+        bool operator !=(const Direction& d) const noexcept {
+            return !(*this == d);
+        }
+
+        // Positionの隣接移動
+        friend Position& operator +=(Position& pos, const Direction& d) noexcept {
+            pos.x += d.dx;
+            pos.y += d.dy;
+            return pos;
+        }
+        friend Position operator +(Position pos, const Direction& d) noexcept {
+            return pos += d;
+        }
+    };
+
+    // 8方向
+    inline static const std::array<Direction, 8> around{{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}};
+
+    // 二つのbit boardで盤面を表す
+    using Board = std::array<std::uint64_t, 2>;
+
+    // 現在のターン
+    Turn turn;
+    // board[0]に黒のbit board, board[1]に白のbit board
+    Board board;
+
+    // 初期盤面
+    State(): turn{}, board{} {
+        put(black, {3, 4});
+        put(black, {4, 3});
+        put(white, {3, 3});
+        put(white, {4, 4});
+    }
+
+    // sが存在するか調べる
+    bool exists(Turn turn, const Position& pos) const {
+        return board[(bool)turn] >> pos.flatten() & 1;
+    }
+
+    // posが空白か調べる
+    bool is_blank(const Position& pos) const {
+        return !(board[0] >> pos.flatten() & 1) && !(board[1] >> pos.flatten() & 1);
+    }
+
+    // posにturnの石を置く
+    void put(Turn turn, const Position& pos) {
+        board[(bool)turn] |= 1ULL << pos.flatten();
+        board[!(bool)turn] &= ~(1ULL << pos.flatten());
+    }
+
+    // 石の数を調べる
+    int count_stone(Turn turn) const {
+        return __builtin_popcountll(board[(bool)turn]);
+    }
+
+    // 全ての石の数を調べる
+    int count_all_stone() const {
+        return count_stone(Turn{false}) + count_stone(Turn{true});
+    }
+
+    // sを置いたときにd方向に裏返せるか
+    bool is_flippable(Turn turn, Position pos, const Direction& d) const {
+        pos += d;
+        if (!(pos.is_in_range() && exists(turn.next(), pos))) return false;
+        do {
+            pos += d;
+        } while (pos.is_in_range() && exists(turn.next(), pos));
+        return pos.is_in_range() && exists(turn, pos);
+    }
+
+    // sを置けるか
+    bool is_putable(Turn turn, const Position& pos) const {
+        if (!is_blank(pos)) return false;
+        for (const auto& d: around) if (is_flippable(turn, pos, d)) return true;
+        return false;
+    }
+
+    // 石をどこかに置けるか
+    bool any_putable(Turn turn) const {
+        for (int i = 0; i < 8; ++i) for (int j = 0; j < 8; ++j) if (is_putable(turn, {i, j})) return true;
+        return false;
+    }
+
+    // 終了盤面か調べる
+    bool has_ended() const {
+        return !any_putable(turn) && any_putable(turn.next());
+    }
+
+    // d方向を裏返す
+    void flip(Turn turn, Position pos, const Direction& d) {
+        for (pos += d; exists(turn.next(), pos); pos += d) {
+            put(turn, pos);
+        }
+    }
+
+    // posに石を置く
+    void move(const Position& pos) {
+        put(turn, pos);
+        for (const auto& d: around) {
+            if (is_flippable(turn, pos, d)) flip(turn, pos, d);
+        }
+        turn.toggle();
+    }
+
+    // posに石を置いた後のStateを返す
+    State moved(const Position& pos) const {
+        State res = *this;
+        res.move(pos);
+        return res;
+    }
+
+    // パスする
+    void pass() noexcept {
+        turn.toggle();
+    }
+
+    // パスした後のStateを返す
+    State passed() const noexcept {
+        State res = *this;
+        res.pass();
+        return res;
+    }
+
+    // 石差を調べる
+    int stone_diff() const {
+        return count_stone(turn) - count_stone(!turn);
+    }
+
+    // 角マスの評価値
+    inline static const std::array<std::array<int, 8>, 8> board_eval{{
+        {120, -40, 20, 5, 5, 20, -40, 120},
+        {-40, -80, -1, -1, -1, -1, -80, -40},
+        {20, -1, 5, 1, 1, 5, -1, 20},
+        {5, -1, 1, 0, 0, 1, -1, 5},
+        {5, -1, 1, 0, 0, 1, -1, 5},
+        {20, -1, 5, 1, 1, 5, -1, 20},
+        {-40, -80, -1, -1, -1, -1, -80, -40},
+        {120, -40, 20, 5, 5, 20, -40, 120}
+    }};
+
+    // 評価関数
+    int eval() const {
+        if (has_ended()) return stone_diff();
+        int res = 0;
+        int cnt = count_all_stone();
+        if (cnt == 63) {
+            for (int i = 0; i < 8; ++i) for (int j = 0; j < 8; ++j) {
+                if (is_putable(turn, {i, j})) return -moved({i, j}).stone_diff();
+                if (is_putable(turn.next(), {i, j})) return passed().moved({i, j}).stone_diff();
+            }
+        }
+        if (cnt >= 57) {
+            res += 10 * (count_stone(turn) - count_stone(turn.next()));
+            for (const auto& pos: corners) {
+                Direction dx{}, dy{};
+                if (pos.x == 0) dx.dx = 1;
+                else dx.dx = -1;
+                if (pos.y == 0) dy.dy = 1;
+                else dy.dy = -1;
+                if (exists(turn, pos)) {
+                    res += 8;
+                    for (Position p = pos + dx; p.is_in_range() && exists(turn, p); p += dx) res += 8;
+                    for (Position p = pos + dy; p.is_in_range() && exists(turn, p); p += dy) res += 8;
+                } else if (exists(turn.next(), pos)) {
+                    res -= 8;
+                    for (Position p = pos + dx; p.is_in_range() && exists(turn.next(), p); p += dx) res -= 8;
+                    for (Position p = pos + dy; p.is_in_range() && exists(turn.next(), p); p += dy) res -= 8;
+                }
+            }
+            if (!any_putable(turn)) res -= 80;
+            return res;
+        }
+        res = 0;
+        for (int i = 0; i < 8; ++i) for (int j = 0; j < 8; ++j) {
+            if (exists(turn, {i, j})) res += board_eval[i][j];
+            else if (exists(turn.next(), {i, j})) res -= board_eval[i][j];
+        }
+        for (const auto& pos: corners) {
+            Direction dx{}, dy{};
+            if (pos.x == 0) dx.dx = 1;
+            else dx.dx = -1;
+            if (pos.y == 0) dy.dy = 1;
+            else dy.dy = -1;
+            if (exists(turn, pos)) {
+                for (Position p = pos + dx; p.is_in_range() && exists(turn, p); p += dx) res += 10 - board_eval[p.x][p.y];
+                for (Position p = pos + dy; p.is_in_range() && exists(turn, p); p += dy) res += 10 - board_eval[p.x][p.y];
+            } else if (exists(turn.next(), pos)) {
+                for (Position p = pos + dx; p.is_in_range() && exists(turn.next(), p); p += dx) res -= 10 - board_eval[p.x][p.y];
+                for (Position p = pos + dy; p.is_in_range() && exists(turn.next(), p); p += dy) res -= 10 - board_eval[p.x][p.y];
+            }
+        }
+        bool flag = true;
+        for (int i = 0; i < 64; ++i) if (is_putable(turn, Position{i})) {
+            flag = false;
+            res += 15;
+        }
+        if (flag) res -= 80;
+        for (int i = 0; i < 64; ++i) if (is_putable(turn.next(), Position{i})) {
+            res -= 15;
+        }
+        return res;
+    }
+
+    void show() const {
+        std::cerr << "  ";
+        for (int i = 0; i < 8; ++i) std::cerr << (char)('a' + i) << " \n"[i == 7];
+        for (int i = 0; i < 8; ++i) {
+            std::cerr << i + 1 << ' ';
+            for (int j = 0; ; ++j) {
+                if (exists(black, {i, j})) std::cerr << '0';
+                else if (exists(white, {i, j})) std::cerr << '1';
+                else if (is_putable(turn, {i, j})) std::cerr << '.';
+                else std::cerr << ' ';
+                if (j != 7) std::cerr << ' ';
+                else {
+                    std::cerr << '\n';
+                    break;
+                }
+            }
+        }
+    }
+
+    // 比較関数
+    bool operator ==(const State& state) const noexcept {
+        return turn == state.turn && board == state.board;
+    }
+    bool operator !=(const State& state) const noexcept {
+        return !(*this == state);
+    }
+};
+
+// ハッシュ
+template<>
+struct std::hash<State> {
+private:
+    std::hash<std::uint64_t> hasher;
+public:
+    std::size_t operator ()(const State& state) const noexcept {
+        std::size_t seed = (bool)state.turn;
+        seed ^= hasher(state.board[0]) + 0x9e3779b97f4a7c15LU + (seed << 12) + (seed >> 4);
+        seed ^= hasher(state.board[1]) + 0x9e3779b97f4a7c15LU + (seed << 12) + (seed >> 4);
+        return seed;
+    }
+};
